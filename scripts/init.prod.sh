3287f815566f094ed8e09e05dc84fc3fe41d8846
François Mathieu <maps_red@yahoo.fr>
2019-03-10 16:50:31 +0100

    Fix scripts

--- scripts/init.prod.sh
+++ scripts/init.prod.sh
@@ -2,6 +2,11 @@
 
 remotes=('api' 'website')
 
+if [[ $PWD != "WowCollection" ]]; then
+	echo "ERROR ! You need to run this script from the WowCollection folder."
+	exit
+fi
+
 for remote in ${remotes[@]}; do
 	if [[ ! -d ${remote} ]]; then
 		git clone git@github.com:WowCollection/${remote}.git
