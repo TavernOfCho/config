fb53d918aa2a5bc136af57bbc0687c9948252063
François MATHIEU <maps_red@yahoo.fr>
2019-03-19 12:56:18 +0100

    Update remote and folder name with new project name

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,13 +2,13 @@
 
 remotes=('client' 'api' 'website' 'admin')
 
-if [[ $(basename $PWD) != "WowCollection" ]]; then
-	echo "ERROR ! You need to run this script from the WowCollection folder."
+if [[ $(basename $PWD) != "TavernOfCho" ]]; then
+	echo "ERROR ! You need to run this script from the TavernOfCho folder."
 	exit
 fi
 
 for remote in ${remotes[@]}; do
 	if [[ ! -d ${remote} ]]; then
-		git clone git@github.com:WowCollection/${remote}.git
+		git clone git@github.com:TavernOfCho/${remote}.git
 	fi
 done
